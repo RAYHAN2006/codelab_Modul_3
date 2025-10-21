068e764e380cc30ae408668c05f20b9029ffeb7b
RAYHAN2006 <maulanarayhan190@gmail.com>
2025-10-21 08:36:58 +0700

    ini adalah luaslingkaran

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double r = 7;
+        double luas = 3.14 * r * r;
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
